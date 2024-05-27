f9346df30568ac030792a125d7073a98484463b2
SOPHIA <sophia.tosar@sou.inteli.edu.br>
2024-05-27 09:22:44 -0300

    Feat: add testes x-Unit, NUnit, MSTest

---
+++ Aplicando-teste/Aplicando-teste/ConversorDeTemperatura.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura
+{
+    public static class ConversorTemperatura
+    {
+        public static double FahrenheitParaCelsius(double temperatura)
+        {
+            return (temperatura - 32) / 1.8; // Simulação de falha
+/*            return Math.Round((temperatura - 32) / 1.8, 2);
+*/        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temperatura em Fahrenheit:");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celsius = ConversorTemperatura.FahrenheitParaCelsius(fahrenheit);
+            Console.WriteLine($"{fahrenheit}°F é equivalente a {celsius}°C.");
+        }
+    }
+}
